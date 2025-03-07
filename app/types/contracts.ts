955a9d26271593bdfabaca66b17d9f6fb655330e
Sanjay Kumar Sah <sanjaykumar.sah@zuru.com>
2025-03-07 16:06:11 +0530

    remove expo app

--- app/types/contracts.ts
+++
@@ -1,9 +0,0 @@
-import { ethers } from 'ethers';
-
-export type FitChainRewards = ethers.Contract & {
-  rewardUser: (user: string, steps: number) => Promise<void>;
-};
-
-export type FitChainNFT = ethers.Contract & {
-  mintBadge: (user: string) => Promise<void>;
-};
